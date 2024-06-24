2b9fd7ac0987313fe195d7155d0a4bc4f29bcd0c
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-06-23 22:27:34 -0800

    Create test-repo4.py

---
+++ test-repo4.py
@@ -0,0 +1,9 @@
+# divide two integer numbers in python
+first = int(input("Enter your first num: "))
+second = int(input("Enter your second num: "))
+
+# To divide two integers
+divv = first / second
+
+# To print the result
+print("The result of two num: " + str(divv))
